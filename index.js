ea187c6d12a74c01a6149cc84501e66bd1ca79e2
Saadi-Rahman <sadi6280@gmail.com>
2022-11-21 06:55:00 +0600

    get bookings of the provided date

--- index.js
+++ index.js
@@ -19,9 +19,22 @@ async function run(){
     const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
     const bookingsCollection = client.db('doctorsPortal').collection('bookings');
 
+    // Use Aggregate to query multiple collections and then merge data
     app.get('/appointmentOptions', async(req, res) => {
+      const date = req.query.date;
       const query = {};
       const options = await appointmentOptionCollection.find(query).toArray();
+
+      // get bookings of the provided date
+      const bookingQuery = {appointmentDate: date}
+      const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+      // code carefully :D
+      options.forEach(option => {
+        const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+        const bookedSlots = optionBooked.map(book => book.slot);
+        const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot));
+        option.slots = remainingSlots;
+      })
       res.send(options);
     })
 
@@ -36,7 +49,6 @@ async function run(){
 
     app.post('/bookings', async(req, res) =>{
       const booking = req.body;
-      console.log(booking);
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
     })
