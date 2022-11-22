6ffdb0d73154ea4c721abaad7c75fd285826f3ff
Saadi-Rahman <sadi6280@gmail.com>
2022-11-22 23:30:55 +0600

    added user API to get all users list from MongoDB

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rf7qboo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send('unauthorized access');
+  }
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+    if(err){
+      return res.status(403).send({message: 'forbidden access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
 async function run(){
   try{
     const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
@@ -49,8 +66,14 @@ async function run(){
      * app.delete('/bookings/:id')
      */
 
-    app.get('/bookings', async(req, res) =>{
+    app.get('/bookings', verifyJWT, async(req, res) =>{
       const email = req.query.email;
+      const decodedEmail = req.decoded.email;
+
+      if(email !== decodedEmail){
+        return res.status(403).send({message: 'forbidden access'});
+      }
+
       const query = { email: email };
       const bookings = await bookingsCollection.find(query).toArray();
       res.send(bookings);
@@ -83,6 +106,12 @@ async function run(){
         return res.send({accessToken: token});
       }
       res.status(403).send({accessToken: ''})
+    })
+
+    app.get('/users', async(req, res) =>{
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      res.send(users);
     })
 
     app.post('/users', async(req, res) =>{
