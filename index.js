ea2ab156182c6bddd3c6afc24a8b6239b437a113
Saadi-Rahman <sadi6280@gmail.com>
2022-11-22 18:41:28 +0600

    create a jwt token and add a get API to use it on the client side

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -73,9 +74,19 @@ async function run(){
       res.send(result);
     });
 
+    app.get('/jwt', async(req, res) =>{
+      const email = req.query.email;
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      if(user){
+        const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'})
+        return res.send({accessToken: token});
+      }
+      res.status(403).send({accessToken: ''})
+    })
+
     app.post('/users', async(req, res) =>{
       const user = req.body;
-      console.log(user);
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
